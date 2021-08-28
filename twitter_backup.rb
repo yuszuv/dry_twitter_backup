2345a59003bc60b579250e26c540a43fecf8ce62
Jan Paki <jan@sternprodukt.de>
2021-08-28 05:13:05 +0200

    clean up

--- twitter_backup.rb
+++ twitter_backup.rb
@@ -1,53 +1,20 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'bundler/setup'
-
-require 'concurrent'
-require 'dotenv/load'
-require 'byebug'
-require "twitter"
-require "json"
-require 'dry/effects'
-require "dry/monads"
-require "dry/files"
-require "dry/cli"
-require "dry/transformer/all"
+require "rubygems"
 require "bundler/setup"
 
+require "byebug"
+require "concurrent"
+require "dotenv/load"
+require "dry/cli"
+require "dry/effects"
+require "dry/files"
+require "dry/monads"
+require "dry/transformer/all"
+require "json"
+require "twitter"
+
 module TwitterBackup
-  module CLI
-    module Commands
-      extend Dry::CLI::Registry
-
-      class CreateBackup < Dry::CLI::Command
-        include Dry::Monads[:result]
-
-        desc "Create a backup of all twitter setup (followers, ...)"
-
-        def call(*)
-          case Actions::DoBackup.new.()
-            in Success() | Success(_) then puts "Yeah 🎉"
-            in Failure(x) then puts "ERROR: #{x.message}"
-            in Failure[error_code, *payload] then p [error_code, payload]
-            in x then p x
-          end
-        end
-      end
-
-      class Version < Dry::CLI::Command
-        desc "Print version"
-
-        def call(*)
-          puts "1.0.0"
-        end
-      end
-
-      register "version", Version, aliases: ["v", "-v", "--version"]
-      register "create",  CreateBackup
-    end
-  end
-
   module Functions
     extend Dry::Transformer::Registry
 
@@ -57,13 +24,14 @@ module TwitterBackup
 
   module Actions
     class DoBackup
+      include Dry::Monads[:do, :result, :try, :task]
+
       include Dry::Effects::Handler.State(:fetching_data)
       include Dry::Effects.State(:fetching_data)
       include Dry::Effects::Handler.State(:writing_data)
       include Dry::Effects.State(:writing_data)
       include Dry::Effects.Defer
-      include Dry::Effects::Handler.Defer #(executor: :immediate)
-      include Dry::Monads[:do, :result, :try, :task]
+      include Dry::Effects::Handler.Defer
 
       def call
         # dry-effects to the rescue: printing dots while waiting for api request to finish
@@ -72,7 +40,6 @@ module TwitterBackup
           is_fetching, result = with_fetching_data(true) do
             defer do
               j = yield fetch_data.fmap(&data_mapper)
-              # j = [{ name: 'bar' }]
               self.fetching_data = false
               j
             end
@@ -80,7 +47,7 @@ module TwitterBackup
 
           defer { print_dots(is_fetching) }
 
-          wait(result).tap{ puts ' done' }
+          wait(result).tap { puts ' done' }
         end
 
         print "Writing to disk ..."
@@ -126,10 +93,10 @@ module TwitterBackup
 
       def write_to_file(blob)
         Try[Dry::Files::IOError] do
-          f    = Dry::Files.new
+          f = Dry::Files.new
 
           writer = f.method(:write)
-          path   = f.expand_path( "./twitter_backup.json", __dir__)
+          path   = f.expand_path( "./backup.json", __dir__)
           output = JSON.pretty_generate(blob)
 
           writer.(path, output)
@@ -148,6 +115,36 @@ module TwitterBackup
       def t(*args)
         Functions[*args]
       end
+    end
+  end
+
+  module CLI
+    module Commands
+      extend Dry::CLI::Registry
+
+      class CreateBackup < Dry::CLI::Command
+        include Dry::Monads[:result]
+
+        desc "Create a backup of all twitter setup (followers, ...)"
+
+        def call(*)
+          case Actions::DoBackup.new.()
+            in Success() | Success(_) then puts "Yeah 🎉"
+            in Failure(x) then puts "\nERROR: #{x.message}"
+          end
+        end
+      end
+
+      class Version < Dry::CLI::Command
+        desc "Print version"
+
+        def call(*)
+          puts "1.1.0"
+        end
+      end
+
+      register "version", Version, aliases: ["v", "-v", "--version"]
+      register "create",  CreateBackup
     end
   end
 end
